ffc53466ab5f66e7b7d8bd8d33b9d36076d7887e
h <intern.ndhuy@cmc.com.vn>
2020-02-27 14:22:38 +0700

    done

---
+++ src/component/Challenge1/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import './index.css';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+const axios = require('axios');
+var params = {
+    access_key: 'bceec5cb6ece63e53832c24907591e29',
+    query: 'Ha Noi'
+}
+class Challenge1 extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            searchName: '',
+            data:{
+                current:{
+                    weather_icons:[],
+                    weather_descriptions:[]
+                }
+
+            }
+        }
+
+    }
+    getData = () => {
+        params.query = this.state.searchName;
+        axios.get('http://api.weatherstack.com/current', { params })
+            .then(response => {
+                const apiResponse = response.data;
+                console.log(response.data);
+                console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+                this.setState({
+                    ...this.state,
+                    data:response.data
+                })
+             
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+    _onChange(event, _type) {
+        this.setState({
+            searchName: event.target.value
+        })
+    }
+    renderCity() {
+        if(this.state.data.location)
+        return (<div className="container">
+            <Card style={{width:'50%',margin:'5% auto'}}>
+                <Card.Header>{this.state.data.location.name} - {this.state.data.location.country}</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Temperature: {this.state.data.current.temperature}</ListGroup.Item>
+                    <ListGroup.Item>Weather Icon: {this.state.data.current.weather_icons.map((data, index) => <img key={index} src={data} />)}</ListGroup.Item>
+                    <ListGroup.Item>Weather Description: {this.state.data.current.weather_descriptions.map((data, index) => <p key={index}>{data}</p>)}</ListGroup.Item>
+                    <ListGroup.Item>Wind degree: {this.state.data.current.wind_degree}</ListGroup.Item>
+                    <ListGroup.Item>Wind dir: {this.state.data.current.wind_dir}</ListGroup.Item>
+                    <ListGroup.Item>Wind speed: {this.state.data.current.wind_speed}</ListGroup.Item>
+                    <ListGroup.Item>Humidity: {this.state.data.current.humidity}</ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </div>)
+        else return <div className="container"><p>Mời bạn nhập</p></div>
+    }
+    render() {
+        return (<div className="container">
+            <div className="challenge1">
+                <p><b>Challenge 1:</b> </p>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Nhập tên thành phố: </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="City Name"
+                        name='searchName'
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        value={this.state.searchName}
+                        onChange={(event) => this._onChange(event, 'searchName')}
+                    />
+                </InputGroup>
+                <Button variant="primary" onClick={this.getData.bind()}>Tìm kiếm</Button>
+            </div>
+            <div className="">{this.renderCity()}</div>
+        </div>)
+    }
+}
+export default Challenge1
---
+++ src/component/Challenge2/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+class Challenge2 extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            fromNumber: 0,
+            toNumber: 0,
+            data:[],
+            index:0
+        
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({ [evt.target.name]: evt.target.value });
+    }
+    getData() {
+        let array = [];
+        if(this.state.fromNumber < this.state.toNumber){
+            for(let i = this.state.fromNumber; i< this.state.toNumber;i++){
+              
+                if (this.kiem_tra_snt(i)==1){
+                    array.push(i)
+                }
+            }
+            this.setState({
+                ...this.state,
+                data: array,
+                index:1
+            })
+        }else {
+            this.setState({
+                ...this.state,
+                data: null,
+                index:0
+            })
+        } 
+    }
+    renderResult() {
+        if (this.state.index == 1){
+        return (<div className="container">
+            {this.state.data.map((item,index) =><p key={index}>{item}</p>)}
+        </div>)
+        }
+        else return (<div><p>Mời bạn nhập</p></div>)
+    }
+    kiem_tra_snt(n) {
+        var flag = true;
+        if (n < 2) {
+            flag = false;
+        }
+        else {
+            for (var i = 2; i < n - 1; i++) {
+                if (n % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag == true) {
+            return 1;
+        }
+        else {
+           return 0;
+        }
+    }
+    render() {
+        return (<div className="container">
+            <div className="challenge2">
+                <p><b>Challenge 2: </b></p>
+                <div>
+                    <p>Khoảng bạn chọn:</p>
+                    <InputGroup className="mb-3">
+
+
+                        <FormControl
+                            placeholder="Từ"
+                            name='fromNumber'
+                            onChange={this.handleChange}
+                        />
+                        <FormControl
+                            placeholder="Đến"
+                            name='toNumber'
+                            onChange={this.handleChange}
+                        />
+
+                    </InputGroup>
+                    <Button variant="primary" onClick={this.getData.bind(this)}>Submit</Button>
+
+                </div>
+                <div>{this.renderResult()}</div>
+            </div>
+        </div>)
+    }
+}
+export default Challenge2
